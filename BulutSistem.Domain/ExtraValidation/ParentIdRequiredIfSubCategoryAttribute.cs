43826f404f048700af747012670ca366f9bb11c1
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-21 18:42:31 +0300

    clean code gereksiz namesapaceler kaldırıldı.

--- BulutSistem.Domain/ExtraValidation/ParentIdRequiredIfSubCategoryAttribute.cs
+++ BulutSistem.Domain/ExtraValidation/ParentIdRequiredIfSubCategoryAttribute.cs
@@ -1,10 +1,6 @@
 ﻿using BulutSistem.Domain.Models;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace BulutSistem.Appllication.Features
 {
@@ -12,20 +8,14 @@ namespace BulutSistem.Appllication.Features
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            // Kategori modeline erişim sağlamak
             var category = validationContext.ObjectInstance as Category;
-
-            // category nesnesinin null olup olmadığını kontrol edelim
             if (category != null)
             {
-                // Eğer kategori bir alt kategori ise ve ParentId değeri null ise hata döndürelim
                 if (category.IsSubCategory && !category.ParentId.HasValue)
                 {
                     return new ValidationResult("Alt kategori ekleniyorsa, parent_id belirtilmelidir.");
                 }
             }
-
-            // Eğer doğrulama başarılıysa, ValidationResult.Success döndürelim
             return ValidationResult.Success;
         }
     }

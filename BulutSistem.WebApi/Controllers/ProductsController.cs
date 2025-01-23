63ab6de3d9f018b5a4b196de52d8a40652bd3a50
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-23 15:14:58 +0300

    redia adn log

--- BulutSistem.WebApi/Controllers/ProductsController.cs
+++ BulutSistem.WebApi/Controllers/ProductsController.cs
@@ -1,11 +1,52 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BulutSistem.Appllication.Features.Categories.DeleteCategory;
+using BulutSistem.Appllication.Features.Products.AddProduct;
+using BulutSistem.Appllication.Features.Products.DeleteProduct;
+using BulutSistem.Appllication.Features.Products.GetProduct;
+using BulutSistem.Appllication.Features.Products.UpdateProduct;
+using BulutSistem.WebApi.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulutSistem.WebApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    [AllowAnonymous]
+    public class ProductsController : ApiController
     {
+        public ProductsController(IMediator mediator) : base(mediator)
+        {
+
+        }
+       
+        [HttpPost]
+        public async Task<IActionResult> Create(AddProductCommand request, CancellationToken cancellationToken)
+        {
+
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllProductQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductById(int id, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(new DeleteProductByIdCommand(id), cancellationToken);
+            return StatusCode(response.StatusCode, response);
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductById(UpdateProductByIdCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+
+        }
+
     }
 }
--- BulutSistem.WebApi/Controllers/VariantsController.cs
+++ BulutSistem.WebApi/Controllers/VariantsController.cs
@@ -1,11 +1,49 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BulutSistem.Appllication.Features.Products.UpdateProduct;
+using BulutSistem.Appllication.Features.Variants.AddVariants;
+using BulutSistem.Appllication.Features.Variants.DeleteVaritants;
+using BulutSistem.Appllication.Features.Variants.GetAllVariants;
+using BulutSistem.Appllication.Features.Variants.UpdateVaritants;
+using BulutSistem.WebApi.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulutSistem.WebApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class VariantsController : ControllerBase
+    [AllowAnonymous]
+    public class VariantsController : ApiController
     {
+        protected VariantsController(IMediator mediator) : base(mediator)
+        {
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(AddVariantCommand request, CancellationToken cancellationToken)
+        {
+
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllVariantQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVariantById(int id, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(new DeleteVariantByIdCommand(id), cancellationToken);
+            return StatusCode(response.StatusCode, response);
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateVariantById(UpdateVariantByIdCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+
+        }
     }
 }
